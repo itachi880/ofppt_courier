e9d38ca5ed83a7dd966026b0adeed0aa0c884831
itachi880 <142436626+itachi880@users.noreply.github.com>
2024-12-10 14:45:26 +0100

    added .env

--- back_end/database/index.js
+++ back_end/database/index.js
@@ -1,8 +1,8 @@
 const mysql=require('mysql2/promise')
 const cox= mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'ofppt_couriers',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port:process.env.DB_PORT,
 })
 module.exports = cox;
--- back_end/index.js
+++ back_end/index.js
@@ -1,12 +1,19 @@
+require("dotenv").config();
 const express=require('express')
 const app=express()
-const cors=require('cors')
+const cors=require('cors');
+const cox = require('./database');
 app.use(cors({
     origin: "*"
 }));
-app.get('/test',(req,res)=>{
-    res.send("hello")
+app.get('/',async (req,res)=>{
+    try{
+        res.json({data:await cox.query('SELECT name FROM sys.databases')[0]})
+    }
+    catch(e){
+        res.json(e)
+    }
 })
-app.listen(4000,()=>{
-    console.log("serveur running at port 4000")
+app.listen(80,()=>{
+    console.log("serveur running at port 80")
 })

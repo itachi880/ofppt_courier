229ce5e77d8d09ee37a4b584749a4e55bd8f8e6a
iliass7777 <iliasstranquille@gmail.com>
2024-12-14 13:36:45 +0100

    twest ili

--- back_end/index.js
+++ back_end/index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mailer =require('./utils/mailer')
 const cors = require("cors");
 app.use(
   cors({
     origin: "*",
   })
 );
+
 app.listen(80, () => {
   console.log("serveur running at port 80");
 });

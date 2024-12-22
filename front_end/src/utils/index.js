432c481d23c66ebcabae3e41bde8e12d92648375
itachi880 <142436626+itachi880@users.noreply.github.com>
2024-12-22 22:22:53 +0100

    Refactor calendar styles: remove old CSS file and add new styles for Big Calendar component

--- front_end/src/utils/index.js
+++ front_end/src/utils/index.js
@@ -1,9 +1,8 @@
 import "react-calendar/dist/Calendar.css";
-import "./calendar.css";
 import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from "moment";
-import "./calendar.css";
+// import "./calender.css";
 const localizer = momentLocalizer(moment);
 
 export function Calendar({

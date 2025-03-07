4b489ba508bedf4523b2173a187e2e80ec5e0713
itachi880 <142436626+itachi880@users.noreply.github.com>
2025-03-07 13:02:56 +0000

    feat: implement HTTPS support with certificate handling and update fetch URLs to use HTTPS

--- back_end/index.js
+++ back_end/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 require("./cron-job").cron();
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const app = express();
 const cors = require("cors");
 const UsersRoute = require("./Routes/Users");
@@ -16,12 +18,13 @@ const {
   APP_LINKS,
   USE_DEV,
 } = require("./utils");
+const path = require("path");
 //back end app;
 if (!USE_DEV) {
   fetch(process.env.SRC_LINKS_APPS + "/back_end")
     .then(async (e) => await e.text())
     .then((e) => {
-      APP_LINKS.BACK_END = "http://" + e;
+      APP_LINKS.BACK_END = "https://" + e;
     })
     .catch((e) => {
       APP_LINKS.BACK_END = "http://localhost:4000";
@@ -30,7 +33,7 @@ if (!USE_DEV) {
   fetch(process.env.SRC_LINKS_APPS + "/front_end")
     .then(async (e) => await e.text())
     .then((e) => {
-      APP_LINKS.FRONT_END = "http://" + e;
+      APP_LINKS.FRONT_END = "https://" + e;
     })
     .catch((e) => {
       APP_LINKS.FRONT_END = "http://localhost:3000";
@@ -50,6 +53,17 @@ app.use("/login", LoginRoute);
 app.use("/groups", GroupRoute);
 app.use("/register", inscriptionRoute);
 app.use(express.static(__dirname + "/data"));
-app.listen(process.env.APP_PORT, () => {
-  console.log("serveur running at port " + process.env.APP_PORT);
-});
+
+if (!USE_DEV) {
+  const options = {
+    key: fs.readFileSync(path.join(__dirname, "certs", "key.pem")), // Your private key
+    cert: fs.readFileSync(path.join(__dirname, "certs", "cert.pem")), // Your certificate
+  };
+  https.createServer(options, app).listen(process.env.APP_PORT, () => {
+    console.log("HTTPS Server running on port " + process.env.APP_PORT);
+  });
+} else {
+  app.listen(process.env.APP_PORT, () =>
+    console.log("running http: " + process.env.APP_PORT)
+  );
+}
